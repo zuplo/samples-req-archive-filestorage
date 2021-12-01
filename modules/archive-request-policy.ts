2794419c106b5432f26b6cec115a68680ce1eee4
Josh Twist <josh@molmorg.com>
2021-11-30 19:02:05 -0800

    initial

---
+++ modules/archive-request-policy.ts
@@ -0,0 +1,40 @@
+import { ZuploRequest } from "@zuplo/runtime";
+
+export type RequestArchivePolicyOptions = {
+	blobContainerPath: string;
+	blobCreateSas: string;
+}
+
+export default async function (request: ZuploRequest, options: RequestArchivePolicyOptions) {
+	// because we will read the body, we need to 
+	// create a clone of this request first, otherwise
+	// there may be two attempts to read the body
+	// causing a runtime error
+	const clone = request.clone();
+	const body = await clone.text();
+
+	// let's generate a unique blob name based on the date and requestId
+	const blobName = `${Date.now()}-${request.requestId}.req.txt`;
+
+	const url = `${options.blobContainerPath}/${blobName}?${options.blobCreateSas}`;
+
+	const result = await fetch(url, {
+		method: "PUT",
+		body: body,
+		headers: {
+			"x-ms-blob-type": "BlockBlob",
+		}
+	});
+
+	if (result.status > 201) {
+		const err = {
+			message: `Error archiving file`,
+			status: result.status,
+			body: await result.text()
+		}
+		request.logger.error(err);
+	}
+
+	// continue
+	return request;
+}
---
+++ modules/hello-world.ts
@@ -0,0 +1,14 @@
+import { ZuploRequest } from "@zuplo/runtime";
+
+export default async function (request: ZuploRequest) {
+  // Use the built in logging infrastructure available
+  // on the request object for extra logging magic
+  // when testing your API
+  request.logger.info(`Hello from inside your Zup`);
+
+  // Zuplo wants to make it easy to build great APIs
+  // You can return an instance of Response or, if
+  // you return another primitive, we'll do our best
+  // to convert it to JSON for you
+  return "What zup?";
+}
